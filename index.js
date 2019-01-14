9c0faaa6764b15dbe383eba51266feb51607fba5
Peter-G-Stone <peter.stone.online@gmail.com>
2019-01-14 12:37:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file
+function findMatching(drivers, name){
+    return drivers.filter (function(driverName) { return driverName.toLowerCase() === name.toLowerCase()});
+
+}
+
+function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
+  
+    for (var i = 0; i < a.length; ++i) {
+      if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
+  
+function fuzzyMatch(drivers, str){
+    
+   let len = str.length;
+
+    return drivers.filter(function(name){
+        return name.slice(0, len) === str;
+    });
+}
+
+function matchName(drivers, name){
+    return drivers.filter( function(driver) {
+        return driver.name === name;
+    });
+}
